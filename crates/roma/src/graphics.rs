bb709f73309dc2fbc43031cd03e2f61a46831b8a
guidota <guido@tamborindeguy.com.ar>
2023-06-03 17:21:04 -0300

    feat: depth testing

--- crates/roma/src/graphics.rs
+++ crates/roma/src/graphics.rs
@@ -3,7 +3,10 @@ use std::{collections::VecDeque, iter::once};
 use wgpu::*;
 use winit::{window::*, *};
 
-use crate::render::sprite_batch::{prepare_sprite_data, SpriteBatchRenderPass, SpriteData, Vertex};
+use crate::{
+    render::sprite_batch::{prepare_sprite_data, SpriteBatchRenderPass, SpriteData, Vertex},
+    resources::{self, texture},
+};
 
 use self::{
     camera::{Camera, DeviceCameraExt},
@@ -23,6 +26,8 @@ pub struct Graphics {
     size: dpi::PhysicalSize<u32>,
     window: Window,
     camera: Camera,
+
+    depth_texture: texture::Texture,
 
     frame_draws: VecDeque<SpriteData>,
     frame_vertices: Vec<Vertex>,
@@ -95,6 +100,9 @@ impl Graphics {
             &config,
             &[&textures.bind_group_layout, &camera.bind_group_layout],
         );
+
+        let depth_texture =
+            resources::texture::Texture::create_depth_texture(&device, &config, "depth_texture");
         Self {
             window,
             surface,
@@ -107,6 +115,7 @@ impl Graphics {
             sprite_batch_pass,
             camera,
             frame_vertices,
+            depth_texture,
         }
     }
 
@@ -123,6 +132,11 @@ impl Graphics {
             self.size = *new_size;
             self.config.width = new_size.width;
             self.config.height = new_size.height;
+            self.depth_texture = resources::texture::Texture::create_depth_texture(
+                &self.device,
+                &self.config,
+                "depth_texture",
+            );
             self.surface.configure(&self.device, &self.config);
         }
     }
@@ -180,7 +194,15 @@ impl Graphics {
                         store: true,
                     },
                 })],
-                depth_stencil_attachment: None,
+                depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
+                    view: &self.depth_texture.view,
+                    depth_ops: Some(wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(0.0),
+                        store: true,
+                    }),
+
+                    stencil_ops: None,
+                }),
             });
 
             render_pass.set_pipeline(&self.sprite_batch_pass.pipeline);
